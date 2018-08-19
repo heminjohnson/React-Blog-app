268e2b0fa27b6d05a38b1483cf19ee8cb43e622a
Hemin Johnson <hemin.johnson@mieterengel.de>
2018-08-20 00:08:50 +0200

    fix: fix the bug in fetching the action

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,7 +9,7 @@ class PostsIndex extends Component {
   }
 
   renderPosts() {
-    _.map(this.props.posts, post => {
+    return _.map(this.props.posts, post => {
       return (
         <li className='list-group-item' key={post.id}>
           {post.title}
